2e72d221728b29a687e1fbbe7fe3ead2eebaed0a
Maksym Khmirov <71634234+Makshmir@users.noreply.github.com>
2023-09-09 14:43:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+  
+
+        Console.WriteLine("Уведіть максимальну кількість крапок");
+
+        int maxDots = 0;
+        int diamontsAmount = 0;
+
+        maxDots = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Пари на дощечках:");
+        for (int i = maxDots; i >= 0; i--)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                //Console.WriteLine(i + "  " + j);
+                diamontsAmount += i + j;
+            }
+        }
+        Console.WriteLine("Необхідно діамантів: "+diamontsAmount);
+    }
+}
