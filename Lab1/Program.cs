28b6e7051a7bfc61502bcfce69813f7281bef358
Maksym Khmirov <71634234+Makshmir@users.noreply.github.com>
2023-10-12 11:06:27 +0300

    Adding Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+        string inputFileName = "input.txt";
+        string outputFileName = "output.txt";
+
+        int maxDots = Convert.ToInt32(File.ReadAllText(inputFileName));
+        int diamontsAmount = 0;
+        Console.WriteLine("Максимальна кількість крапок на дощечці: "+maxDots);
+
+
+       //Console.WriteLine("Пари на дощечках:");
+        for (int i = maxDots; i >= 0; i--)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                //Console.WriteLine(i + "  " + j);
+                diamontsAmount += i + j;
+            }
+        }
+        Console.WriteLine("Необхідно діамантів: "+diamontsAmount);
+
+        File.WriteAllText(outputFileName, diamontsAmount.ToString());
+        Console.ReadKey();
+    }
+}
