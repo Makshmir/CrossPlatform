87ba6f71b35f9a4988b149ca1735ae1f158dfa2c
Maksym Khmirov <71634234+Makshmir@users.noreply.github.com>
2023-10-19 14:46:16 +0300

    Added Lab3

---
+++ Lab3(Usage)/Program.cs
@@ -0,0 +1,11 @@
+﻿using NugetPackageForLab3;
+internal class Program
+{
+    static void Main()
+    {
+        var mazeSolver = new NugetPackageForLab3.Class1();
+        int count = mazeSolver.SolveMaze("input.txt", "output.txt");
+        //int count = mazeSolver.SolveMaze("C:\\Users\\user\\source\\repos\\Lab3CrossPlatfUsage\\Lab3CrossPlatfUsage\\bin\\Debug\\net7.0\\input.txt", "C:\\Users\\user\\source\\repos\\Lab3CrossPlatfUsage\\Lab3CrossPlatfUsage\\bin\\Debug\\net7.0\\output.txt");
+        Console.WriteLine($"Number of areas in the maze: {count}");
+    }
+}
